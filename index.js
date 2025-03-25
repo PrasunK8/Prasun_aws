a6728a4136ddd3786ff638cbd27057d1a8dac2d5
PrasunK8 <123256251+PrasunK8@users.noreply.github.com>
2025-03-25 15:51:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello MCKV')
+  res.send('Hii DS student')
 })
 
 app.get('/courses', function (req, res) {
